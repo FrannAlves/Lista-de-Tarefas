d40d656fec695b1c7951bfd78779faf73c70ab97
Francielle <francielle_fs@outlook.com>
2024-01-12 17:23:13 -0300

    Adiciona a funcionalidade de listar tarefas

--- src/controladores/tarefas.js
+++ src/controladores/tarefas.js
@@ -1,26 +1,26 @@
 const knex = require('../conexoes/conexao');
 
-const cadastrarTarefa = async (req, res) =>{
-    const {titulo, descricao, prioridade, concluida, prazo_conclusao} = req.body;
+const cadastrarTarefa = async (req, res) => {
+    const { titulo, descricao, prioridade, concluida, prazo_conclusao } = req.body;
 
     try {
-        const tarefaExiste = await knex('tarefas').where({titulo}).first();
+        const tarefaExiste = await knex('tarefas').where({ titulo }).first();
 
-    if(tarefaExiste){
-        return res.status(400).json({
-            mensagem: 'Já existe uma tarefa cadastrada com esse título.'
-        });
-    }
+        if (tarefaExiste) {
+            return res.status(400).json({
+                mensagem: 'Já existe uma tarefa cadastrada com esse título.'
+            });
+        }
 
-    const tarefa = await knex('tarefas').insert({
-        titulo,
-        descricao,
-        prioridade,
-        concluida,
-        prazo_conclusao
-    }). returning('*');
+        const tarefa = await knex('tarefas').insert({
+            titulo,
+            descricao,
+            prioridade,
+            concluida,
+            prazo_conclusao
+        }).returning('*');
 
-    return res.status(201).json(tarefa);
+        return res.status(201).json(tarefa);
 
     } catch (error) {
         return res.status(500).json({
@@ -30,6 +30,23 @@ const cadastrarTarefa = async (req, res) =>{
 }
 
 
-module.exports = {
-    cadastrarTarefa
+const listarTarefas = async (req, res) => {
+
+    try {
+        const tarefas = await knex('tarefas').orderBy('id', 'asc');
+
+        return res.status(200).json(tarefas);
+
+    } catch (error) {
+        return res.status(500).json({
+            mensagem: 'Erro interno do servidor.'
+        });
+    }
 }
+
+
+
+module.exports = {
+    cadastrarTarefa,
+    listarTarefas
+};
--- src/rotas.js
+++ src/rotas.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const { cadastrarUsuario } = require('./controladores/usuarios');
 const { login } = require('./controladores/autenticacao');
-const { cadastrarTarefa } = require('./controladores/tarefas');
+const { cadastrarTarefa, listarTarefas } = require('./controladores/tarefas');
 const validarRequisicao = require('./intermediarios/validarRequisicao');
 const { filtroLogin } = require('./intermediarios/filtroLogin');
 const usuarioSchema = require('./validacoes/usuarioSchema');
@@ -20,6 +20,6 @@ rotas.post('/login', validarRequisicao(loginSchema), login);
 rotas.use(filtroLogin);
 
 rotas.post('/tarefa', validarRequisicao(tarefaSchema), cadastrarTarefa);
-
+rotas.get('/tarefa', listarTarefas);
 
 module.exports = rotas;
