93f00c9d895ea321fbd6e733fdfc6b53f6a6074d
Farhan Reza <108195448+farhan7reza7@users.noreply.github.com>
2024-01-29 02:26:50 +0530

    Updated index.js for following the best practices
    Updated index.js for following the best practices

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
+// Importing necessary dependencies
 require('dotenv').config();
-
 const express = require('express');
+
+// Importing route handlers from 'rotas' module
 const rotas = require('./rotas');
 
+// Creating an Express application
 const app = express();
 
+// Middleware to parse JSON requests
 app.use(express.json());
+
+// Using the route handlers defined in 'rotas'
 app.use(rotas);
 
+// Starting the server and listening on the specified port or defaulting to 3000
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`Rodando na porta ${process.env.PORT}`)
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
